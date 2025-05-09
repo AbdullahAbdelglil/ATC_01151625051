a5ca0f47dc77b786fa29c13824d0292482270a34
AbdullahAbdelglil <abdoullahzaky1@gmail.com>
2025-05-09 04:37:15 +0300

    feat: login & refresh token
    - Add /login endpoint to Authentication controller
    - Add login function to AuthenticationService
    - Add generateRefreshToken to JwtService

--- FullStackEventBookingSystem/src/main/java/com/eventbooking/service/impl/AuthenticationServiceImpl.java
+++ FullStackEventBookingSystem/src/main/java/com/eventbooking/service/impl/AuthenticationServiceImpl.java
@@ -1,14 +1,20 @@
 package com.eventbooking.service.impl;
 
 import com.eventbooking.service.AuthenticationService;
-import com.eventbooking.service.EventService;
+import com.eventbooking.service.JWTService;
 import com.eventbooking.service.UserService;
+import com.eventbooking.service.dto.JwtAuthenticationResponse;
+import com.eventbooking.service.dto.SignInRequestDTO;
 import com.eventbooking.service.dto.SignUpRequestDTO;
 import com.eventbooking.service.dto.UserDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
@@ -17,19 +23,46 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JWTService jwtService;
 
     public AuthenticationServiceImpl(UserService userService,
-                                     PasswordEncoder passwordEncoder) {
+                                     PasswordEncoder passwordEncoder,
+                                     AuthenticationManager authenticationManager,
+                                     JWTService jwtService) {
         this.userService = userService;
         this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
     }
 
-    public UserDTO signUp(SignUpRequestDTO signUpRequestDTO) {
+    @Override
+    public UserDTO register(SignUpRequestDTO signUpRequestDTO) {
         log.debug("signing Up new User UserRequestDTO {}", signUpRequestDTO);
 
         // Hash the password first
         signUpRequestDTO.setPassword(passwordEncoder.encode(signUpRequestDTO.getPassword()));
         UserDTO userDTO = new UserDTO(signUpRequestDTO);
         return userService.save(userDTO);
+    }
+
+    @Override
+    public JwtAuthenticationResponse login(SignInRequestDTO signInRequestDTO) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        signInRequestDTO.getEmail(),
+                        signInRequestDTO.getPassword())
+        );
+
+        UserDTO user = userService.findOne(signInRequestDTO.getEmail()).orElseThrow(
+                () -> new IllegalArgumentException("Invalid email or password"));
+
+        String accessToken = jwtService.generateToken(user);
+        String refreshToken = jwtService.generateRefreshToken(new HashMap<>(), user);
+
+        JwtAuthenticationResponse response = new JwtAuthenticationResponse();
+        response.setAccessToken(accessToken);
+        response.setRefreshToken(refreshToken);
+        return response;
     }
 }
